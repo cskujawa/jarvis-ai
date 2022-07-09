51a3b80c9924751cc2680da324b1f42f11df503e
Cole <kujawabusiness@gmail.com>
2022-07-09 14:53:31 +0000

    Initialize repo

---
+++ interface/laravel/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        host: '0.0.0.0',
+        port: 3030
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
